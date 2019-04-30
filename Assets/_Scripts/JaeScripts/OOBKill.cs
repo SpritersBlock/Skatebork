b00bd6953e2a40de80ecb25665ead6d7812bdd38
Jaesda Kim <signupdump1@gmail.com>
2019-04-30 13:10:42 +1200

    I... THINK Analytics for Item Respawns?

--- Assets/_Scripts/JaeScripts/OOBKill.cs
+++ Assets/_Scripts/JaeScripts/OOBKill.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class OOBKill : MonoBehaviour {
 
     public float yLevelKill;
+
+    public AnalyticsTracker at;
 
 	// Use this for initialization
 	void Start () {
@@ -22,6 +25,9 @@ public class OOBKill : MonoBehaviour {
             //transform.position += new Vector3 (0,3,0);
             GetComponent<Rigidbody>().velocity = Vector3.zero;
             transform.position = Vector3.zero;
+            //Analytics.CustomEvent("Item Respawned", new Dictionary<string, int> { {"Sew Time", variable } });
+            at.TriggerEvent();
+            print("RESPAWN");
         }
 	}
 }
