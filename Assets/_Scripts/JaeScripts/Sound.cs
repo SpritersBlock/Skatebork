58f93f1003dd94e1310c39f252156ade22760388
Jaesda Kim <signupdump1@gmail.com>
2019-04-30 12:33:34 +1200

    Audio Manager Scripts Added

---
+++ Assets/_Scripts/JaeScripts/Sound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound : MonoBehaviour {
+
+    public string soundName;
+    
+    public AudioClip clip;
+    [Range(0f, 1f)]
+    public float volume = 1;
+    [Range(-3f, 3f)]
+    public float pitch = 1;
+
+    [HideInInspector]
+    public AudioSource source;
+}
