5bfc57639df9f10f13f91a23d0cbeb6bfb231eaa
Jaesda Kim <signupdump1@gmail.com>
2019-04-30 14:25:19 +1200

    Tutorial Text Added In, very Basic

---
+++ Assets/_Scripts/JaeScripts/TutText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutText : MonoBehaviour {
+
+    void LateUpdate()
+    {
+        var target = Camera.main.transform.position;
+        target.y = transform.position.y;
+        transform.LookAt(target);
+    }
+
+    public void RemoveText()
+    {
+        Destroy(gameObject);
+    }
+}
