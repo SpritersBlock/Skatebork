6da12ff205fcbf85126bb10359bf643279b0c9f4
Jaesda Kim <signupdump1@gmail.com>
2019-05-03 14:08:18 +1200

    Added Music Manager

---
+++ Assets/_Scripts/JaeScripts/MusicManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public static bool mmExists;
+
+    public AudioSource[] musicTracks;
+
+    public int currentTrack;
+
+    public bool musicCanPlay;
+
+	// Use this for initialization
+	void Awake () {
+        if (!mmExists)
+        {
+            mmExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (musicCanPlay)
+        {
+            if (!musicTracks[currentTrack].isPlaying)
+            {
+                musicTracks[currentTrack].Play();
+            }
+        } else
+        {
+            musicTracks[currentTrack].Stop();
+        }
+	}
+
+    public void SwitchTrack(int newTrack)
+    {
+        musicTracks[currentTrack].Stop();
+        currentTrack = newTrack;
+        musicTracks[currentTrack].Play();
+    }
+}
---
+++ Assets/_Scripts/JaeScripts/MusicSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSwitcher : MonoBehaviour {
+
+    private MusicManager theMM;
+    public int newTrack;
+    public bool switchOnStart;
+
+	// Use this for initialization
+	void Start () {
+        theMM = FindObjectOfType<MusicManager>();
+
+        if (switchOnStart)
+        {
+            theMM.SwitchTrack(newTrack);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
