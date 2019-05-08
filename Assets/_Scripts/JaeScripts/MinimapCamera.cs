a7184ac986e4c6d3c89f438fd0288664e7142a24
Jaesda Kim <signupdump1@gmail.com>
2019-05-08 13:13:43 +1200

    Added Minimap

---
+++ Assets/_Scripts/JaeScripts/MinimapCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour {
+
+    public Transform target;
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = target.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+        
+        transform.rotation = Quaternion.Euler(90, target.eulerAngles.y, 0);
+    }
+}
