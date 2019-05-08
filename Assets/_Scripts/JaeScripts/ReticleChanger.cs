5505b5c824d77cd252e5f1a5c25105d32f3b0adc
Jaesda Kim <signupdump1@gmail.com>
2019-05-08 13:51:53 +1200

    Added ReticleChanger script

---
+++ Assets/_Scripts/JaeScripts/ReticleChanger.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReticleChanger : MonoBehaviour {
+
+    public Image reticle;
+    public Color reticleActive;
+    public Color reticleInactive;
+
+    bool reticleOn;
+
+    public PlayerMovement_2 player;
+
+    private void Start()
+    {
+        ReticleOff();
+    }
+
+    private void Update()
+    {
+        if (player.hasFood)
+        {
+            if (!reticleOn)
+            {
+                ReticleOn();
+            }
+        }
+        else
+        {
+            if (reticleOn)
+            {
+                ReticleOff();
+            }
+        }
+    }
+
+    void ReticleOn()
+    {
+        reticleOn = true;
+        reticle.color = reticleActive;
+    }
+
+    void ReticleOff()
+    {
+        reticleOn = false;
+        reticle.color = reticleInactive;
+    }
+}
