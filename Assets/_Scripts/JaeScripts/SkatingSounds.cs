f33ae525f7a73dc0ebfb789cd32f1c394ede3847
Jaesda Kim <signupdump1@gmail.com>
2019-05-10 13:27:19 +1200

    Replaced Skating Sound

--- Assets/_Scripts/JaeScripts/SkatingSounds.cs
+++ Assets/_Scripts/JaeScripts/SkatingSounds.cs
@@ -20,7 +20,7 @@ public class SkatingSounds : MonoBehaviour {
         currentSpeed = new Vector2(player.controller.velocity.x, player.controller.velocity.z).magnitude;
         if (player.controller.isGrounded && player.gm.playerHealth > 0 && player.gm.gameOn)
         {
-            skateSound.volume = (currentSpeed / 20);
+            skateSound.volume = (currentSpeed / 19);
         }
         else
         {
