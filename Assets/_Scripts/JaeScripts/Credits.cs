c1e8dd5c6fa9d50e57d690f2c4c1e4f7c7aeef98
Jaesda Kim <signupdump1@gmail.com>
2019-05-16 12:09:25 +1200

    Finished Credits

--- Assets/_Scripts/JaeScripts/Credits.cs
+++ Assets/_Scripts/JaeScripts/Credits.cs
@@ -5,12 +5,18 @@ using UnityEngine.UI;
 
 public class Credits : MonoBehaviour {
 
-    public Text[] creditsTexts1;
-    public Text[] creditsTexts2;
+    public GameObject creditsTexts1;
+    public GameObject creditsTexts2;
+
+    public Text buttonText;
+
+    public int creditsIndex;
 
 	// Use this for initialization
 	void Start () {
-        
+        creditsTexts2.SetActive(false);
+        buttonText.text = "Assets";
+        creditsIndex = 1;
     }
 	
 	// Update is called once per frame
@@ -20,6 +26,19 @@ public class Credits : MonoBehaviour {
 
     public void SwitchCredits()
     {
-
+        if (creditsIndex == 1)
+        {
+            creditsIndex = 2;
+            creditsTexts1.SetActive(false);
+            creditsTexts2.SetActive(true);
+            buttonText.text = "Devs";
+        }
+        else if (creditsIndex == 2)
+        {
+            creditsIndex = 1;
+            creditsTexts1.SetActive(true);
+            creditsTexts2.SetActive(false);
+            buttonText.text = "Assets";
+        }
     }
 }
