ffa1713df1dcca057c40f08b8f1bef22cdd1b17b
Jaesda Kim <signupdump1@gmail.com>
2019-05-17 13:08:23 +1200

    Added Randomised Death Text

---
+++ Assets/_Scripts/JaeScripts/RandomDeathText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomDeathText : MonoBehaviour {
+
+    [TextArea(3,10)]
+    public string[] randomText;
+    public Text deathText;
+
+	// Use this for initialization
+	void Start () {
+        deathText.text = randomText[Random.Range(0, randomText.Length)];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
