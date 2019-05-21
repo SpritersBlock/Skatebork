0ca434708572f49fc1004c230551035bd4e6b5d7
Jaesda Kim <signupdump1@gmail.com>
2019-05-21 13:24:55 +1200

    ACTUALLY Replace Sun

---
+++ Assets/_Scripts/JaeScripts/FaceCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour {
+
+    void LateUpdate()
+    {
+        var target = Camera.main.transform.position;
+        transform.LookAt(target);
+    }
+}
