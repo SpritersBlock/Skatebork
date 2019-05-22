ad12d9f0fd8c164e1f7eb4ca791fa267fa030afa
Jaesda Kim <signupdump1@gmail.com>
2019-05-22 14:25:37 +1200

    Added Squash and Stretch to MM Dog

--- Assets/_Scripts/JaeScripts/RandomSpin.cs
+++ Assets/_Scripts/JaeScripts/RandomSpin.cs
@@ -6,6 +6,7 @@ public class RandomSpin : MonoBehaviour {
     
     public GameObject playerNull;
     //Rigidbody rb;
+    Animator anim;
 
     public float spinSpeed;
 
@@ -13,18 +14,23 @@ public class RandomSpin : MonoBehaviour {
 	void Start () {
         //rb = playerNull.GetComponent<Rigidbody>();
         //rb.angularVelocity = new Vector3(0, spinSpeed, 0);
+        anim = playerNull.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Equals))
+        if (spinSpeed != 1)
         {
-            UpdateDogSpeed(spinSpeed += 1);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Minus))
+            if (!anim.GetBool("Spinning"))
+            {
+                anim.SetBool("Spinning", true);
+            }
+        } else
         {
-            UpdateDogSpeed(spinSpeed -= 1);
+            if (anim.GetBool("Spinning"))
+            {
+                anim.SetBool("Spinning", false);
+            }
         }
 
         playerNull.transform.eulerAngles += new Vector3(0, spinSpeed * Time.timeScale, 0);
@@ -34,5 +40,20 @@ public class RandomSpin : MonoBehaviour {
     {
         //rb.angularVelocity = new Vector3(0, newSpeed, 0);
         spinSpeed = newSpeed;
+        if (newSpeed > 7)
+        {
+            anim.SetFloat("SquashAmount", 1);
+        } else if (newSpeed > 4)
+        {
+            anim.SetFloat("SquashAmount", 0.66f);
+        }
+        else if (newSpeed > 1)
+        {
+            anim.SetFloat("SquashAmount", 0.33f);
+        }
+        else
+        {
+            anim.SetFloat("SquashAmount", 0f);
+        }
     }
 }
