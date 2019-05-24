e83dddffa9889d7559cd2dacf508d7ab8bc1ab41
Jaesda Kim <signupdump1@gmail.com>
2019-05-24 12:47:57 +1200

    Added Falling Food to Main Menu

---
+++ Assets/_Scripts/JaeScripts/MenuFoodSpawner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuFoodSpawner : MonoBehaviour {
+
+    public GameObject[] itemArray;
+    public float itemSpawnSpeed;
+    public float itemLaunchSpeed;
+
+    float timer;
+    public float timerMax;
+
+    // Use this for initialization
+    void Start () {
+        timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        
+        if (timer < timerMax)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (timer >= timerMax)
+        {
+            SpawnItem(transform);
+            timer = 0;
+        }
+	}
+
+    public void SpawnItem(Transform spawnTransform)
+    {
+        GameObject itemClone;
+        itemClone = Instantiate(itemArray[Random.Range(0, itemArray.Length)], spawnTransform.position, spawnTransform.rotation);
+        Rigidbody rb = itemClone.GetComponent<Rigidbody>();
+        rb.velocity += (Vector3.up * itemSpawnSpeed);
+        rb.velocity += new Vector3(Random.Range(-itemLaunchSpeed, itemLaunchSpeed) * itemSpawnSpeed, 0, Random.Range(-itemLaunchSpeed, itemLaunchSpeed) * itemSpawnSpeed);
+        
+    }
+}
---
+++ Assets/_Scripts/JaeScripts/MenuOOBKill.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuOOBKill : MonoBehaviour {
+
+    public float yLevelKill;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), Random.Range(-3, 3));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (transform.position.y <= yLevelKill)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
